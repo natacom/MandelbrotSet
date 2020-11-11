b65a8c60843610e3c0db14baf3fe28b0e5483a2a
natacom <4712112+natacom@users.noreply.github.com>
2020-11-11 23:59:42 +0900

    Add DoubleConverter

---
+++ MandelbrotSet/DoubleConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace MandelbrotSet
+{
+    class DoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is double)) return null;
+
+            string output = ((double)value).ToString();
+
+            if (!output.Contains('.')) {
+                output += ".0";
+            }
+
+            return output;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = value as string;
+
+            if (!string.IsNullOrEmpty(str) && double.TryParse(str, out double newValue)) {
+                return newValue;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
