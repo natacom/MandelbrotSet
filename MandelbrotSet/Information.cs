112819a3c9efe8915a71687b10454b305d2209f9
natacom <4712112+natacom@users.noreply.github.com>
2020-11-14 17:30:27 +0900

    Add reset timer for Information.

--- MandelbrotSet/Information.cs
+++ MandelbrotSet/Information.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Timers;
 
 namespace MandelbrotSet
 {
@@ -8,7 +9,24 @@ namespace MandelbrotSet
         public event PropertyChangedEventHandler PropertyChanged;
         private void Notify([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
+        private static readonly string s_defInfoStr = "No Error";
         private string m_infoString= string.Empty;
+
+        private Timer m_timer = new Timer(5000);
+
+        public Information()
+        {
+            m_timer.AutoReset = false;
+            m_timer.Elapsed += (sending, arg) =>
+            {
+                InfoString = s_defInfoStr;
+            };
+        }
+
+        ~Information()
+        {
+            m_timer?.Dispose();
+        }
 
         public string InfoString
         {
@@ -17,6 +35,8 @@ namespace MandelbrotSet
             {
                 m_infoString = value;
                 Notify();
+
+                m_timer.Start();
             }
         }
     }
